424c93af4c63e1d48acbc21ffdeb3ff1140ebf33
Sho Takemori <stakemorii@gmail.com>
2015-02-27 11:32:23 +0900

    add compute_lift_and_non_lift.

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8; mode: sage -*-
-from sage.all import QQ, PolynomialRing, FiniteField
+from sage.all import QQ, PolynomialRing, FiniteField, NumberField
 from sage.rings.number_field.number_field import NumberField_absolute
 import os
 
@@ -33,3 +33,37 @@ def check_cong(p, t2_eigenvalue, lift, non_lift, space):
     assert all(mod_p(b)%p == 0 for b in v)
 
 data_dir = os.path.join(os.getenv("HOME"), "ksr_lift_data_dir")
+
+def _lift_chr_ply():
+    x = PolynomialRing(QQ, names="x").gens()[0]
+    d = {16: x + 4078080,
+         18: x + 8785920,
+         20: x + 383331840}
+    return d
+
+
+def fname(f):
+    return os.path.join(data_dir, f)
+
+
+def _lift_name(k):
+    return fname("lift_%s_%s_prec6.sobj"%(str(k+1), str(k-2)))
+
+
+def _nonlift_name(k):
+    return fname("nonlift_%s_%s_prec6.sobj"%(str(k+1), str(k-2)))
+
+
+def compute_lift_and_non_lift(space, prec):
+    k = space.wt - 1
+    lift_name = _lift_name(k)
+    nonlift_name = _nonlift_name(k)
+    lift_chr_ply = _lift_chr_ply()[k]
+    lift_ev = -lift_chr_ply.constant_coefficient()
+    R = PolynomialRing(QQ, names="x")
+    K = NumberField(R((space.hecke_charpoly(2)/lift_chr_ply)), names="a")
+    nonlift_ev = K.gens()[0]
+    lift = space.eigenform_with_eigenvalue_t2(lift_ev)
+    lift.save_as_binary(lift_name)
+    nonlift = space.eigenform_with_eigenvalue_t2(nonlift_ev)
+    nonlift.save_as_binary(nonlift_name)
